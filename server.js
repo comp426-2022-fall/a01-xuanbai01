fdea98a60189929442c47f7dc5e1d90ef4f5ed68
shawnbai <xuanbai@ad.unc.edu>
2022-09-01 01:14:02 -0400

    fix return data try04

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ fs.readFile(fileName, (err, data) => {
   const server = http.createServer((req, res) => {
         res.statusCode = 200
         res.setHeader('Content-Type', 'text/html')
-        res.end(data
+        res.end(data)
   })
 
   server.listen(port, () => {
